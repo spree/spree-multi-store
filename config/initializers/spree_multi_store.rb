7c6d4c730b64dc71c7d9db4642eaa9c4cacfa3e0
Damian Legawiec <damian@getvendo.com>
2026-03-31 10:44:24 +0200

    Move setting `Spree.root_domain` earlier in the chain

--- config/initializers/spree_multi_store.rb
+++ config/initializers/spree_multi_store.rb
@@ -1,3 +1,8 @@
+# Multi-store setup
+# You need to set a wildcard `root_domain` on the store to enable multi-store setup
+# all new stores will be created in a subdomain of the root domain, eg. store1.localhost, store2.localhost, etc.
+Spree.root_domain = ENV.fetch('SPREE_ROOT_DOMAIN', 'localhost')
+
 Rails.application.config.after_initialize do
   Spree::Dependencies.current_store_finder = 'Spree::Stores::FindCurrent'
 
@@ -11,9 +16,4 @@ Rails.application.config.after_initialize do
   if defined?(Spree::Admin)
     Spree.admin.partials.product_form_sidebar << 'spree/admin/products/form/stores'
   end
-
-  # Multi-store setup
-  # You need to set a wildcard `root_domain` on the store to enable multi-store setup
-  # all new stores will be created in a subdomain of the root domain, eg. store1.localhost, store2.localhost, etc.
-  Spree.root_domain = ENV.fetch('SPREE_ROOT_DOMAIN', 'localhost')
 end
